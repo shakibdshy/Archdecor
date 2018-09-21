697204bfa32b81f91d76b167f3069a880dd1067a
shakibdshy <shakibdshy@gmail.com>
2018-09-21 17:52:38 +0600

    Slider style

--- active.js
+++ active.js
@@ -15,4 +15,47 @@
          }
 
      });
+
+    //  Slider Active
+    $('.slider-active').owlCarousel({
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })(jQuery);
